07a8322c6726106ba41aed6d19ad462e79588f21
rakeshlookings <rakesh@lookings.in>
2022-03-31 01:04:55 +0530

    removed routes

--- src/routes.js
+++ src/routes.js
@@ -17,13 +17,8 @@
 
 */
 import Dashboard from "views/Dashboard.js";
-import Notifications from "views/Notifications.js";
-import Icons from "views/Icons.js";
-import Typography from "views/Typography.js";
-import TableList from "views/Tables.js";
-import Maps from "views/Map.js";
 import UserPage from "views/User.js";
-import UpgradeToPro from "views/Upgrade.js";
+
 
 var routes = [
   {
